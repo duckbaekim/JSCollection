1d329fcea0d4b7346718c36a07138efbb974804d
inqu <inqu@inquui-MacBookAir.local>
2021-07-15 05:54:20 -0700

    Feat : 배열함수 추가, 맵 함수 추가, 셋 함수 추가

--- ArrayFc.js
+++ ArrayFc.js
@@ -1,4 +1,51 @@
 const testArr = [{name : 'inqu', age : 30}, {name:'test', age: 31}];
+const team = [
+    {
+        name: 'melinda',
+        position: 'ux designer'
+    },
+    {
+        name : 'katie',
+        position:'stategist'
+    },
+    {
+        name:'madhavi',
+        position:'developer'
+    },
+    {
+        name:'justi혀n',
+        position:'manager'
+    },
+    {
+        name:'chris',
+        position:'developer'
+    }
+]
 
-const age30 = testArr.filter(el=>el.age ===30);
-console.log(age30);
+//map() 길이 유지, 배열 형태변경
+const prices = ['1,0','흥정가능', '2.15']
+const formattedPrices = prices.map(price=>parseFloat(price));
+console.log(formattedPrices)
+//[1, NaN, 2.15]
+
+
+//sort() 순서 변경, 형태 & 길이 유지
+
+
+//filter() 길이 변경, 형태 유지
+const filterPrice = formattedPrices.filter(price=>price)
+//[1, 2.15]
+
+
+//find() 1개 데이터 반환, 형태 유지
+const findChris = team.find(user => user.name ==='chris')
+console.log(findChris);
+
+
+//forEach() 형태 이용, 리턴 없음
+
+//reduce() 길이 형태 모두 변경 가능
+
+
+
+
--- MapFc.js
+++ MapFc.js
@@ -3,6 +3,7 @@ let mapTest = new Map();
 
 //추가
 mapTest.set('견종', '래브라도레트리버')
+
 //chaining
 mapTest.set('크기', '대형견').set('색상', '갈색')
 
@@ -12,9 +13,12 @@ mapTest.get('견종');
 //삭제
 mapTest.delete('견종');
 
-
 //key반환
 mapTest.keys(); //map iteraiter
 
+//존재확인 
+mapTest.has('견종');
+
 //
+
 console.log(mapTest);
