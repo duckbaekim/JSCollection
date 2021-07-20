9030c0a8dc365661989bde90718e88a7fd83d2c0
inqu <inqu16@gmail.com>
2021-07-20 19:17:26 +0900

    Feat: Promisejs 추가

---
+++ Promise.js
@@ -0,0 +1,56 @@
+//init
+function getUserPreferences(){
+    const preferences = new Promise((resolve, reject)=>{
+        resolve({
+            theme: 'dusk',
+        });
+    });
+    return preferences;
+}
+
+function failUserPreference(){
+    const finder = new Promise((resolve, reject) => {
+        reject({
+            type: '접근 거부됨',
+        })
+    })
+    return finder;
+}
+
+//호출
+getUserPreferences().then(preferences=>{
+    console.log(preferences.theme);
+})
+
+failUserPreference().then(preferences=>{
+    //실행 안됨
+    console.log(preferences.theme);
+})
+.catch(error=>{
+    console.log(`실패: ${error.type}`)
+})
+
+//중첩된 Promise
+function getMusic(theme){
+    if(theme==='dusk'){
+        return Promise.resolve({
+            album:'music for airports',
+        })
+    }
+    return Promise.resolve({
+        album:'kind of blue',
+    })
+}
+
+getUserPreferences().then(preferences => {
+    return getMusic(preferences.theme);
+}).then(music=> {
+    console.log(music.album);
+}).catch(e=>{
+    console.log(e)
+})
+
+//refactoring
+getUserPreferences().then(preferences => getMusic(preferences.theme))
+.then(music=>{console.log(music.album)})
+.catch(e=>{console.log(e)})
